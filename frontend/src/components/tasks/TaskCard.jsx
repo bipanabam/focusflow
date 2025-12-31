bc621ce7939ca4180e693fcb263740e8467a23b6
bipanabam <bipanabam147@gmail.com>
2025-12-31 21:03:36 +0545

    TaskCard updated

--- frontend/src/components/tasks/TaskCard.jsx
+++ frontend/src/components/tasks/TaskCard.jsx
@@ -1,42 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
 import { BiSolidEdit } from "react-icons/bi";
 
+import { TASK_CATEGORY, TASK_PRIORITY } from "../../constants/taskUI";
+
 const TaskCard = ({ task }) => {
-    const categoryColors = {
-        Work: "bg-blue-100 text-blue-700 dark:bg-blue-900 dark:text-blue-200",
-        Team: "bg-purple-100 text-purple-700 dark:bg-purple-900 dark:text-purple-200",
-        Personal: "bg-green-100 text-green-700 dark:bg-green-900 dark:text-green-200",
-        Admin: "bg-orange-100 text-orange-700 dark:bg-orange-900 dark:text-orange-200",
-    };
-
-    const statusColors = {
-        "completed": "bg-green-100 text-green-700 dark:bg-green-900 dark:text-green-200",
-        "in_progress": "bg-yellow-100 text-yellow-700 dark:bg-yellow-900 dark:text-yellow-200",
-        "pending" : "bg-gray-100 text-gray-700 dark:bg-gray-700 dark:text-gray-200",
-    };
-
-    const priorityColors = {
-        high: "text-red-600 dark:text-red-400",
-        medium: "text-yellow-600 dark:text-yellow-400",
-        low: "text-green-600 dark:text-green-400"
-    };
     const navigate = useNavigate();
 
     return (
         <div className="bg-white dark:bg-gray-800 rounded-xl p-4 shadow-sm border border-gray-200 dark:border-gray-700 hover:shadow-md transition">
             
             <div className="flex items-start justify-between mb-3">
-                {/* <span className={`text-xs font-semibold px-2 py-1 rounded-full ${categoryColors[task.category] || categoryColors.Work}`}>
+                <span className={`text-xs font-semibold px-2 py-1 rounded-full ${TASK_CATEGORY[task.category] || TASK_CATEGORY.work}`}>
                     {task.category}
-                </span> */}
-                <span className={`text-xs font-semibold px-2 py-1 rounded-full ${priorityColors[task.priority]}`}>
+                </span>
+                <span className={`text-xs font-semibold px-2 py-1 rounded-full ${TASK_PRIORITY[task.priority]}`}>
                     {task.priority.toUpperCase()}
                 </span>
-                <span className={`text-xs font-semibold px-2 py-1 rounded-full ${statusColors[task.status]}`}>
+                {/* <span className={`text-xs font-semibold px-2 py-1 rounded-full ${TASK_STATUS[task.status]}`}>
                     {task.status === "completed" ? "Completed" : task.status === "in_progress" ? "Active" : "Pending"}
-                </span>
+                </span> */}
             </div>
 
             <h4 className="text-sm font-semibold text-gray-900 dark:text-white px-2 mb-3 line-clamp-2">
